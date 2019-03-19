e3f2280816f7015cb7977710f9d985ce92568f7b
Rahul Wasnik <rahulz.wasnik@gmail.com>
2019-03-20 02:11:02 +1100

    Adding test cases for the header component

--- src/app/feature/home/home-page/home-page.component.spec.ts
+++ src/app/feature/home/home-page/home-page.component.spec.ts
@@ -3,9 +3,11 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { Router } from "@angular/router";
 import { By } from '@angular/platform-browser';
 import { CommonModule, Location } from '@angular/common';
+import { NO_ERRORS_SCHEMA } from "@angular/core";
 
 import { HomePageComponent } from './home-page.component';
-import { BookDetailsPageComponent } from './../../book-details/book-details-page/book-details-page.component';
+import { RouteConstant } from './../../../shared/constant/route.constant';
+import { BookListingsComponent } from './../../book-details/book-listings/book-listings.component';
 
 describe('HomePageComponent', () => {
   let component: HomePageComponent;
@@ -15,14 +17,14 @@ describe('HomePageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [HomePageComponent, BookDetailsPageComponent],
+      declarations: [HomePageComponent, BookListingsComponent],
       imports: [
-
         CommonModule,
         RouterTestingModule.withRoutes([
-          { path: 'book/listings', component: BookDetailsPageComponent }
+          { path: RouteConstant.book + '/' + RouteConstant.listings, component: BookListingsComponent }
         ])
-      ]
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
       .compileComponents();
   }));
@@ -43,12 +45,15 @@ describe('HomePageComponent', () => {
     expect(fixture.debugElement.nativeElement.querySelector('#homeContent').innerText).toContain('Welcome to a book app...Start by viewing our collection')
   });
 
-  it('should go to book listing page when the View all the books button is clicked', async(() => {
+  it('should display a button which takes the user to book listing page', () => {
+    expect(fixture.debugElement.nativeElement.querySelector('a').href).toContain(RouteConstant.book + '/' + RouteConstant.listings)
+  });
+
+  it('should navigate to book listing page when the View all the books button is clicked', async(() => {
     fixture.debugElement.query(By.css('a')).nativeElement.click();
     fixture.whenStable().then(() => {
-      expect(location.path()).toEqual('/book/listings');
+      expect(location.path()).toEqual(RouteConstant.book + '/' + RouteConstant.listings);
     });
-
   }));
 
 });
